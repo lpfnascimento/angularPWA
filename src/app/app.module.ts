b568c0643176bd8dfb95bc81f8213bd1f4a17773
Liliane Nascimento <liliane.nascimento@eldorado.org.br>
2022-11-11 09:56:57 -0300

    testando branch develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+//declara para angular que é uma classe componente
+@Component({
+        //declaração de meta-dados
+        //template equivalente a view
+        selector: '[app-topo]',
+        templateUrl: './topo.component.html',
+        styleUrls:['./topo.component.css']
+})
+export class TopoComponent {
+
+}
